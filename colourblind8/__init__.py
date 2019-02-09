e85b47e4d0663043183f51baa2315e6f67301a69
Hayley Boyce <hayleyfboyce@gmail.com>
2019-02-08 17:28:01 -0800

    added structure and functions

---
+++ colourblind8/__init__.py
@@ -0,0 +1,1 @@
+from Colourblind8.Colourblind8 import set_deutera, set_prota, set_trita
---
+++ colourblind8/colourblind8.py
@@ -0,0 +1,61 @@
+class Colourblind8:
+    def __ini__(self):
+        '''This is the Constructor of the class'''
+
+
+
+    def set_deutera(self,plot):
+        '''
+        A function that converts a matplotlib plot to a Deuteranopia friendly palette
+        and theme
+
+        inputs
+        ------
+        plot:  a  matplotlib figure that needs formatting
+
+        outputs:
+        --------
+        new_plot: this is a plot that has been transformed to a Deuteranopia
+                  vision friendly graph, with changes to the background,
+                  gridlines and colour
+
+        '''
+        return new_plot
+
+    def set_prota(self,plot):
+       '''
+        A function that converts a matplotlib plot to a Protanopia friendly palette
+        and theme
+
+        inputs
+        ------
+        plot:  a  matplotlib figure that needs formatting
+
+        outputs:
+        --------
+        new_plot: this is a plot that has been transformed to a Protanopia
+                  vision friendly graph, with changes to the background,
+                  gridlines, and colour
+
+        '''
+        return new_plot
+
+       def set_trita(self,plot):
+       '''
+        A function that converts a matplotlib plot to a Tritanopia friendly palette
+        and theme
+
+        inputs
+        ------
+        plot: a  matplotlib figure that needs formatting
+
+        outputs:
+        --------
+        new_plot: this is a plot that has been transformed to a Tritanopia
+                  vision friendly graph, with changes to the background,
+                  gridlines, and colour
+
+        '''
+        return new_plot
+
+
