cee453c4834f24fb12493ebeeacc343e631732c6
Hayley Boyce <hayleyfboyce@gmail.com>
2019-02-15 15:28:57 -0800

    fixed setup long_description

--- setup.py
+++ setup.py
@@ -5,14 +5,13 @@ setup(
     version='0.1dev',
     packages=['Colourblind8',],
     license='LICENSE.txt',
-    long_description=open('README.txt').read(),
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
      author = ['Ian Flores', 'Sabrina Tse', 'Hayley Boyce '],
     install_requires=[
         "numpy",
-        "pandas",
         "matplotlib",
-        "pytest",
-        
+        "pytest",       
     ]
 )
 
